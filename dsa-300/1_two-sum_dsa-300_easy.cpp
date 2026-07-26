c30401afeda349ef35f07ba826dc53876f686b1e
cod4274-alt <cod4274@gmail.com>
2026-07-26 09:37:29 +0530

    feat(solution): solve Two Sum [Easy]

--- dsa-300/1_two-sum_dsa-300_easy.cpp
+++ dsa-300/1_two-sum_dsa-300_easy.cpp
@@ -2,14 +2,46 @@
  * Problem: Two Sum
  * Unique ID: 1
  * Difficulty: Easy
- * Date: 2026-07-25
+ * Date: 2026-07-26
  * Source: DSA Practice Platform
  */
 
-// Your code here
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
 
 int main() {
-    std::cout << "Hello from C++" << std::endl;
+    int N;
+    cin >> N;
+
+    vector<long long> nums(N);
+    for (int i = 0; i < N; i++) {
+        cin >> nums[i];
+    }
+
+    long long target;
+    cin >> target;
+
+    unordered_map<long long, int> mp;
+
+    for (int i = 0; i < N; i++) {
+        long long complement = target - nums[i];
+
+        if (mp.find(complement) != mp.end()) {
+            int idx1 = mp[complement];
+            int idx2 = i;
+
+            if (idx1 < idx2)
+                cout << idx1 << " " << idx2;
+            else
+                cout << idx2 << " " << idx1;
+
+            return 0;
+        }
+
+        mp[nums[i]] = i;
+    }
+
     return 0;
 }
